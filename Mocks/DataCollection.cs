6e000a22f738dbe796eb7f2740540aad69f6523e
Быков Егор <SEEGa1234@gmail.com>
2024-06-16 01:29:01 +0300

    fix calcHollinger

--- Mocks/DataCollection.cs
+++ Mocks/DataCollection.cs
@@ -13,23 +13,21 @@ namespace DiplomMag.Mocks
             foreach (var item in dataCollect)
             {
                 var dataAnalysis = new DataAnalysis(game, item);
-                item.Statistic.CalcUPer = dataAnalysis.CalcUPer() == double.NaN || dataAnalysis.CalcUPer() < 0 ? 0 : dataAnalysis.CalcUPer();
+                item.Statistic.CalcUPer = double.IsNaN(dataAnalysis.CalcUPer()) || dataAnalysis.CalcUPer() < 0 ? 0 : dataAnalysis.CalcUPer();
             }
             foreach (var item in dataCollect)
             {
                 var dataAnalysis = new DataAnalysis(game, item);
-                item.Statistic.CalcPace = dataAnalysis.CalcPace() == double.NaN ? 0 : dataAnalysis.CalcPace();
+                item.Statistic.CalcPace = double.IsNaN(dataAnalysis.CalcPace()) ? 0 : dataAnalysis.CalcPace();
             }
             foreach (var item in dataCollect)
             {
                 var dataAnalysis = new DataAnalysis(game, item);
-                item.Statistic.CalcHollinger = dataAnalysis.CalcHollinger() == double.NaN || dataAnalysis.CalcHollinger()<0 ? 0 : dataAnalysis.CalcHollinger();
-				item.Statistic.CalcTPA = dataAnalysis.CalcTPA() == double.NaN ? 0 : dataAnalysis.CalcTPA()/1000.0;
-				item.Statistic.CalcOffRating = dataAnalysis.CalcOffRating() == double.NaN ? 0 : dataAnalysis.CalcOffRating()/1000.0;
-				item.Statistic.CalcDefRating = dataAnalysis.CalcDefRating() == double.NaN ? 0 : dataAnalysis.CalcDefRating()/1000.0;
-                var test2 = dataAnalysis.CalcEFGProcent();
+                item.Statistic.CalcHollinger = double.IsNaN(dataAnalysis.CalcHollinger()) || dataAnalysis.CalcHollinger()<0 || item.Statistic.TimePlayed.Minute<10 ? 0 : dataAnalysis.CalcHollinger();
+				item.Statistic.CalcTPA = double.IsNaN(dataAnalysis.CalcTPA()) ? 0 : dataAnalysis.CalcTPA()/1000.0;
+				item.Statistic.CalcOffRating = double.IsNaN(dataAnalysis.CalcOffRating()) ? 0 : dataAnalysis.CalcOffRating()/1000.0;
+				item.Statistic.CalcDefRating = double.IsNaN(dataAnalysis.CalcDefRating()) ? 0 : dataAnalysis.CalcDefRating()/1000.0;
 				item.Statistic.CalcEFGProcent = double.IsNaN(dataAnalysis.CalcEFGProcent()) || double.IsInfinity(dataAnalysis.CalcEFGProcent()) ? 0 : dataAnalysis.CalcEFGProcent();
-				var test = dataAnalysis.CalcTSProcent();
 				item.Statistic.CalcTSProcent = double.IsNaN(dataAnalysis.CalcTSProcent()) || double.IsInfinity(dataAnalysis.CalcTSProcent()) ? 0 : dataAnalysis.CalcTSProcent();
 			}
 
